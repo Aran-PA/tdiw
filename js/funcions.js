fbe3cdae4d746a9eef0434cdfae6ae267281297f
AranPA <aran23448@gmail.com>
2024-09-25 12:29:44 +0200

    first commit p2

--- js/funcions.js
+++ js/funcions.js
@@ -1,3 +1,8 @@
 function confirmaRegistre(){
     //completa
+    alert("Registrant estudiant")
+    console.log("Registrant estudiant")
+    let tagDiv = document.getElementById('formDiv')
+    tagDiv.innerHTML = "<p class='important'>Registrat</p>"
+    return false
 }
